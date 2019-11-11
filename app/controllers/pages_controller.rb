d26802669d691f86d53b0395918a8bee7c65e038
Duval Cash <duvalwcash@gmail.com>
2019-11-11 18:18:03 -0500

    added all routes and respective controller actions, and refactored navbar into partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   def landing
   end
 
+  def how_to_participate
+  end
+
+  def the_rules
+  end
+
+  def sponsors
+  end
+
   def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#landing'
-  post 'landing/create'
+  post 'pages#/create', to: 'pages#create'
+  get 'pages/how_to_participate', to: 'pages#how_to_participate'
+  get 'pages/the_rules', to: 'pages#the_rules'
+  get 'pages/sponsors', to: 'pages#sponsors'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
